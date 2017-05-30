daa0b17391a21e0c69d1d0e7d50ac264e4574dad
dalthviz <d.althviz10@uniandes.edu.co>
2017-05-30 01:35:22 -0500

    Agrega form para busqueda por propiedades nombre, nacionalidad y movimiento.

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,33 +1,49 @@
 class ArtistController < ApplicationController
 
 def index
-   #@artists = Artist.where("SELECT ?label WHERE  { test:thing rdfs:label ?label  FILTER(LANG(?label) = "") }").limit(1).offset(0)
-   # @artists  = Artist.where("DESCRIBE ?uri WHERE { SELECT DISTINCT ?uri (http://dbpedia.org as ?graph) WHERE { GRAPH http://dbpedia.org { ?uri a http://dbpedia.org/ontology/Book . ?uri ?p ?o } } LIMIT 5}")
-
-    #@artists = Artist.where("?uri <http://dbpedia.org/property/name> ?name").limit(1).offset(0).order("DESC(?name)")
-    #@artists = Artist.query('SELECT * WHERE {?s ?p ?o} LIMIT 10','')
-    #@artists = Artist.all.limit(1)
-    #@ric = Artist.find('http://dbpedia.org/page/Fernando_Botero')
-
-    # @artists = Artist.find_by_sparql('select ?uri     WHERE {       GRAPH <http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte>       { ?uri a <http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte#Artist> } } limit 1')
 
     sparql = SPARQL::Client.new("http://www.grupo1.semanticweb.uniandes.edu.co:8890/sparql")
-    
+
     artist = "<http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte#Artist>"
-    
     type = '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'
+
     name = '<http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte#name>'
+    qname = ""
     nacionality = '<http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte#nationality>'
+    qnacionality = ""
+    qoptinalnac = "OPTIONAL"
     movement = '<http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte#movement>'
+    qmovement_name = ""
+    qoptinalmn = "OPTIONAL"
+
+    limit=20
+
+    if params[:name] && params[:name] != ''
+      qname = ', ?name FILTER(?name="'+params[:name]+'"^^<http://www.w3.org/2001/XMLSchema#string>)'
+    end
+    if params[:movement_name] && params[:movement_name] != ''
+      qmovement_name = ', ?movement_name FILTER(?movement_name="'+params[:movement_name]+'"^^<http://www.w3.org/2001/XMLSchema#string>)'
+      qoptinalmn = ""
+    end
+    if params[:nacionality] && params[:nacionality] != ''
+      qnacionality = ', ?nacionality FILTER(?nacionality="'+params[:nacionality]+'"^^<http://www.w3.org/2001/XMLSchema#string>)'
+      qoptinalnac = ""
+    end
+    if params[:limit] && params[:limit] != ''
+      limit = params[:limit]
+    end
+
+
     @q =    "SELECT ?uri ?name ?movement ?movement_name ?nacionality
-            WHERE { ?uri #{type} #{artist} . 
-                ?uri  #{name} ?name .
-                OPTIONAL {?uri #{movement} ?movement } .
-                OPTIONAL {?uri #{nacionality} ?nacionality } .
-                OPTIONAL {?movement #{name} ?movement_name } .} limit 20"
-    
+            WHERE { ?uri #{type} #{artist} .
+                ?uri  #{name} ?name #{qname} .
+                #{qoptinalmn} {?uri #{movement} ?movement } .
+                #{qoptinalnac} {?uri #{nacionality} ?nacionality #{qnacionality} } .
+                #{qoptinalmn} {?movement #{name} ?movement_name #{qmovement_name} } .} limit #{limit}"
+
+
     @query= sparql.query(@q)
-    
+
 
 
 
